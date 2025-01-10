1f36f9d2b87436be4c76c12ee88cff7ecdeb08f4
kvtsdev <epikshibaa@gmail.com>
2025-01-10 12:36:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ document.addEventListener('DOMContentLoaded', () => {
         switch (command) {
             case 'about':
                 typeText(
-                    "I'm a computer science student based in central Europe.\n" +
+                    "I'm a cybersec-engineer, and i enjoy doing networking tools\n" +
                     "I like messing around with pen-testing systems and websites, building tools (mostly in Python), and doing my part to keep the internet a safer place.\n" +
                     "I’m also into teaching others about this stuff, especially how to stay completely anonymous online (paranoia moment 😭).",
                     outputSection
